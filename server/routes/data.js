120b75f6be0a79bf4d94507f3b0e37a9448837bd
daotung8801 <daoxuantung8801@gmail.com>
2021-11-26 22:49:09 +0700

    Add import and export data to mongoDB

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,70 +1,23 @@
+const dataController = require('../controllers/dataController');
 const express = require("express");
 const router = express.Router();
-const csvModel = require("../models/User");
-const xlsx = require("xlsx");
-const csv = require("csvtojson");
-const multer = require("multer");
+
+const multer = require('multer');
+
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "../public/uploads");
+  destination:(req,file,cb)=>{
+    cb(null,'./public/uploads');
   },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
+  filename:(req,file,cb)=>{
+    cb(null,file.originalname);
+  }
 });
 
-const uploads = multer({ storage: storage });
+const uploads = multer({storage:storage});
 
-router.get("/", (req, res) => {
-  csvModel.find((err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      if (data !== "") {
-        res.json(data);
-      } else {
-        res.send("no data");
-      }
-    }
-    return null;
-  });
-});
-
-router.post("/import", uploads.single("csv"), (req, res) => {
-  //convert csvfile to jsonArray
-  console.log("req file", req.file);
-  csv()
-    .fromFile(req.file.path)
-    .then((jsonObj) => {
-      console.log(jsonObj);
-      csvModel.insertMany(jsonObj, (err, data) => {
-        if (err) {
-          console.log(err);
-          res.send("err");
-        } else {
-          res.redirect("data/");
-          return;
-        }
-      });
-    });
-});
-
-router.post("/export", (req, res) => {
-  var wb = xlsx.utils.book_new(); //new workbook
-  csvModel.find((err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      var temp = JSON.stringify(data);
-      temp = JSON.parse(temp);
-      var ws = xlsx.utils.json_to_sheet(temp);
-      var down = process.cwd() + "/public/users.xlsx";
-      xlsx.utils.book_append_sheet(wb, ws, "sheet1");
-      xlsx.writeFile(wb, down);
-      res.download(down);
-    }
-    return null;
-  });
-});
+router.get('/api/data/import', dataController.import);
+router.post('/api/data/importdata', uploads.single('csv'), dataController.importData);
+router.get('/api/data/export', dataController.export);
+router.post('/api/data/exportdata', dataController.exportData);
 
 module.exports = router;
