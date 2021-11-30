3b5973de8a6f96e95f6bbe4e255fd4a3876a85a0
Ngoc Nguyen <30312571+n3377n@users.noreply.github.com>
2021-12-01 00:33:30 +0700

    data controller cleanup

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,23 +1,21 @@
-const dataController = require('../controllers/dataController');
+const dataController = require("../controllers/data");
 const express = require("express");
 const router = express.Router();
 
-const multer = require('multer');
+const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination:(req,file,cb)=>{
-    cb(null,'./public/uploads');
+  destination: (req, file, cb) => {
+    cb(null, "./public/uploads");
   },
-  filename:(req,file,cb)=>{
-    cb(null,file.originalname);
-  }
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
-const uploads = multer({storage:storage});
+const uploads = multer({ storage: storage });
 
-router.get('/import', dataController.import);
-router.post('/importdata', uploads.single('csv'), dataController.importData);
-router.get('/export', dataController.export);
-router.get('/exportdata', dataController.exportData);
+router.post("/import", uploads.single("csv"), dataController.importData);
+router.get("/export", dataController.exportData);
 
 module.exports = router;
