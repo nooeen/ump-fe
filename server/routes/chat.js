1ef70f35aba390e9172ed5939ca5337bcb4af4dc
Ngoc Nguyen <30312571+n3377n@users.noreply.github.com>
2021-12-01 12:24:03 +0700

    Revert "update chat apis"
    This reverts commit ef934e798bf33e4af07a7523fc81ae2f6ce40bc2.

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,12 +1,4 @@
 const express = require("express");
 const router = express.Router();
-const chatController = require("../controllers/chat");
+const forumController = require("../controllers/chat");
 
-router.get("/getMessage", chatController.getMessage);
-router.post("/saveMessage", chatController.saveMessage);
-
-router.get("/twoUserConv", chatController.twoUserConv);
-router.get("/userConv", chatController.userConv);
-router.post("/newConv", chatController.newConv);
-
-module.exports = router;
