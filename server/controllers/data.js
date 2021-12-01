5f6bc3f2ea492329410d9035c37938ff0ba4997b
daotung8801 <daoxuantung8801@gmail.com>
2021-12-01 12:01:19 +0700

    Set cell width when export xlsx file

--- server/controllers/data.js
+++ server/controllers/data.js
@@ -1,22 +1,23 @@
 const userModel = require("../models/User");
 const XLSX = require("xlsx");
+const fs = require("fs");
 
 class DataController {
   // [POST] /api/data/importData
   importData(req, res, next) {
-    // fs.readFile(req.file.path, 'utf8', function (err, data) {
-    //     if (err) throw err;
-    //     console.log(data);
-    //     var json = JSON.parse(data);
-    //
-    //     csvModel.insertMany(json, function (err) {
-    //         if(err){
-    //           console.log(err);
-    //         }else{
-    //           res.redirect('/api/data/import');
-    //         }
-    //     });
-    // });
+    fs.readFile(req.file.path, 'utf8', function (err, data) {
+        if (err) throw err;
+        console.log(data);
+        var json = JSON.parse(data);
+
+      userModel.insertMany(json, function (err) {
+            if(err){
+              console.log(err);
+            }else{
+              res.redirect('/api/data/import');
+            }
+        });
+    });
     res.json("importdata");
   }
 
@@ -34,6 +35,18 @@ class DataController {
           temp = JSON.parse(temp);
           // res.json(temp);
           var ws = XLSX.utils.json_to_sheet(temp);
+          var wscols = [
+            {wch:50},
+            {wch:50},
+            {wch:50},
+            {wch:50},
+            {wch:50},
+            {wch:50},
+            {wch:50},
+            {wch:50},
+          ];
+          ws['!cols'] = wscols;
+
           var down = process.cwd() + "/public/users.xlsx";
           XLSX.utils.book_append_sheet(wb, ws, "sheet1");
           XLSX.writeFile(wb, down);
