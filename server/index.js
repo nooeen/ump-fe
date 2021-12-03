a5ecb0322299186adb2eaa84668f931ba4c4ba51
Pham Vu Minh <vuminhpham2001@gmail.com>
2021-12-04 01:57:29 +0700

    clean up code

--- server/index.js
+++ server/index.js
@@ -4,71 +4,71 @@ const pino = require("express-pino-logger")();
 const route = require("./routes");
 const cors = require("cors");
 const db = require("./db");
-const { disconnect } = require("mongoose");
-// const { createServer } = require("http");
-// const { Server } = require("socket.io");
+const {disconnect} = require("mongoose");
 
 const app = express();
-//const httpServer = createServer(app);
-//const io = new Server(httpServer, { /* options */ });
 const io = require("socket.io")(3002, {
-  cors: {
-    origin: ["http://localhost:3000"],
-  }
+    cors: {
+        origin: ["http://localhost:3000"],
+    }
 });
 const users = {}
 io.on("connection", (socket) => {
-  console.log('\nconnected to socket io')
-  console.log("\nsocket id: ", socket.id)
-  console.log("count client: ", io.engine.clientsCount)
-  
-  socket.on("sendMessage", (message, join) => {
-    console.log("\nsendMessage: ", message)
-    if (join != ""){
-      socket.to(join).emit('receive-message', message)
-    } else {
-      socket.broadcast.emit('receive-message', message)
-    }
-    
-  })
+    console.log('\nconnected to socket io')
+    console.log("\nsocket id: ", socket.id)
+    console.log("count client: ", io.engine.clientsCount)
 
-  socket.on("joinRoom", room => {
-    socket.join(room)
-  })
+    //send and receive messages
+    socket.on("sendMessage", (message, join) => {
+        console.log("\nsendMessage: ", message)
+        if (join != "") {
+            socket.to(join).emit('receive-message', message)
+        } else {
+            socket.broadcast.emit('receive-message', message)
+        }
+    })
+    socket.on("confirmReceived", message => {
+        console.log("confirmReceived: ", message)
+    })
 
-  socket.on("leaveRoom", room => {
-    socket.leave(room)
-  })
+    //display ID
+    socket.on("sendID", id => {
+        socket.emit("idconnect", id)
+    })
 
-  socket.on("sendID", id => {
-    socket.emit("idconnect", id)
-  })
-  socket.on("confirmReceived", message => {
-    console.log("confirmReceived: ", message)
-  })
-  //socket.on('ping', n => console.log(n))
-  socket.on('disconnect', () => {
-    socket.emit('user-disconnected', users[socket.id])
-    console.log("disconnected from socket io")
-  })
+    //join room
+    socket.on("joinRoom", room => {
+        socket.join(room)
+    })
+
+    //leave room
+    socket.on("leaveRoom", room => {
+        socket.leave(room)
+    })
+
+    //disconnect from server (testing purpose)
+    socket.on('disconnect', () => {
+        socket.emit('user-disconnected', users[socket.id])
+        console.log("disconnected from socket io")
+    })
 });
 
 
-var corsOptions = { 
-  origin: "http://localhost:3000",
+var corsOptions = {
+    origin: "http://localhost:3000",
 };
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(pino);
 app.use(cookieParser());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
 });
 app.use(cors(corsOptions));
 
@@ -77,5 +77,5 @@ db.connect();
 route(app);
 
 app.listen(3001, () =>
-  console.log("Express server is running on localhost:3001")
+    console.log("Express server is running on localhost:3001")
 );
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,105 +1,86 @@
 import {io} from "socket.io-client"
 
 export default function Chat() {
-  var messageContainer = document.getElementById('message-container');
-  const socket = io('http://localhost:3002')
-  var id;
-  var message1
-  socket.on('connect', () => {
-    id = socket.id
-    socket.emit("sendID", id)
-  })
-  
-  socket.on("receive-message", message => {
-    message1 = "received: " + message
-    appendMessage(message1)
-    socket.emit("confirmReceived", message)
-  })
-  socket.on("idconnect", id => {
-    message1 = "id: " + id
-    appendMessage(message1)
-  })
+    var messageContainer = document.getElementById('message-container');
+    const socket = io('http://localhost:3002')
+    var id;
+    var message1
 
-  // socket.on('user-disconnected', name => {
-  //   appendMessage('${name} connected')
-  // })
+    //display client's ID
+    socket.on('connect', () => {
+        id = socket.id
+        socket.emit("sendID", id)
+    })
+    socket.on("idconnect", id => {
+        message1 = "id: " + id
+        appendMessage(message1)
+    })
 
-  function send1(e) {
-    
-    e.preventDefault();
-    message1 = "12"
-    socket.emit('sendMessage', "messsage 1")
-  }
-  
-  function send(e) {
-      e.preventDefault();
-      appendMessage( e.target.m1.value)
-      socket.emit('sendMessage', e.target.m1.value, e.target.join.value)
-  }
+    //send and receive messages
+    socket.on("receive-message", message => {
+        message1 = "received: " + message
+        appendMessage(message1)
+        socket.emit("confirmReceived", message)
+    })
 
-  function send2(e) {
-      e.preventDefault();
-      
-      socket.emit('sendMessage', "messsage 2")
-  }
+    function send(e) {
+        e.preventDefault();
+        appendMessage(e.target.m1.value)
+        socket.emit('sendMessage', e.target.m1.value, e.target.join.value)
+    }
 
-  function joinRoom(e) {
-    e.preventDefault();
-    message1 = "join room: " + e.target.room.value
-    appendMessage( message1)
-    socket.emit("joinRoom", e.target.room.value)
-  }
+    //join room
+    function joinRoom(e) {
+        e.preventDefault();
+        message1 = "join room: " + e.target.room.value
+        appendMessage(message1)
+        socket.emit("joinRoom", e.target.room.value)
+    }
 
-  function leaveRoom(e) {
-    e.preventDefault();
-    message1 = "leave room: " + e.target.leave.value
-    appendMessage( message1)
-    socket.emit("leaveRoom", e.target.leave.value)
-  }
+    //leave room
+    function leaveRoom(e) {
+        e.preventDefault();
+        message1 = "leave room: " + e.target.leave.value
+        appendMessage(message1)
+        socket.emit("leaveRoom", e.target.leave.value)
+    }
 
-  function appendMessage(message) {
-    messageContainer = document.getElementById('message-container')
-    const messageElement = document.createElement('div');
-    messageElement.innerText = message;
-    messageContainer.append(messageElement);
-  }
+    //display messages
+    function appendMessage(message) {
+        messageContainer = document.getElementById('message-container')
+        const messageElement = document.createElement('div');
+        messageElement.innerText = message;
+        messageContainer.append(messageElement);
+    }
 
-  // let count = 0;
-  // setInterval(() => {
-  //     socket.emit('ping', ++count)
-  // }, 1000)
+    //hotkeys for disconnect and reconnect (testing purpose)
+    document.addEventListener('keydown', e => {
+        if (e.target.matches('input')) return
+        if (e.key === 'c') socket.connect()
+        if (e.key === 'd') socket.disconnect()
+    })
 
-  document.addEventListener('keydown', e => {
-      if(e.target.matches('input')) return
-      if(e.key === 'c') socket.connect()
-      if(e.key === 'd') socket.disconnect()
-  })
 
-  
-     
-  return (
-    <div>
-      <div id="message-container"></div>
-        <form method="get" name = "form1" id="form1" onSubmit = {send}>
-          <input type = "text" name = "m1"/>
-          <button type="submit" form="form1" value="S1">Send Message To</button>
-          <input type = "text" name = "join"/>
-        </form>
+    return (
+        <div>
+            <div id="message-container"></div>
+            <form method="get" name="form1" id="form1" onSubmit={send}>
+                <input type="text" name="m1"/>
+                <button type="submit" form="form1" value="S1">Send Message To</button>
+                <input type="text" name="join"/>
+            </form>
 
-        <form method="get" name = "form2" id="form2" onSubmit = {joinRoom}>
-          <input type = "text" name = "room"/>
-          <button type="submit" form="form2" value="S2">join room</button>
-        </form>
+            <form method="get" name="form2" id="form2" onSubmit={joinRoom}>
+                <input type="text" name="room"/>
+                <button type="submit" form="form2" value="S2">join room</button>
+            </form>
 
-        <form method="get" name = "form3" id="form3" onSubmit = {leaveRoom}>
-          <input type = "text" name = "leave"/>
-          <button type="submit" form="form3" value="S3">leave room</button>
-        </form>
-
-        <button onClick= {send1}>message 1 </button>
-        <button onClick= {send2}>message 2 </button>
-    </div>
-  );
+            <form method="get" name="form3" id="form3" onSubmit={leaveRoom}>
+                <input type="text" name="leave"/>
+                <button type="submit" form="form3" value="S3">leave room</button>
+            </form>
+        </div>
+    );
 }
 
 
