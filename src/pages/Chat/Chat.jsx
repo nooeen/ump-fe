9c1445f572ef33699ebe055833d3ba8aa4844ef1
Ngoc Nguyen <30312571+n3377n@users.noreply.github.com>
2021-12-05 21:57:11 +0700

    fix double id

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,4 +1,4 @@
-import {io} from "socket.io-client"
+import { io } from "socket.io-client";
 import "./Chat.css";
 import ChatService from "../../services/chat.service";
 import Topbar from "../../components/topbar/Topbar";
@@ -7,189 +7,179 @@ import Message from "../../components/chat/message";
 import axios from "axios";
 
 const API_URL = process.env.REACT_APP_URL;
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
 
 export default function Chat() {
-    var messageContainer = document.getElementById('message-container');
-    const socket = io('http://localhost:3002')
-    var id;
-    var message1
+  var messageContainer = document.getElementById("message-container");
+  const socket = io(SOCKET_URL);
+  var id;
+  var message1;
 
-    //display client's ID
-    socket.on('connect', () => {
-        id = socket.id
-        socket.emit("sendID", id)
-    })
-    socket.on("idconnect", id => {
-        message1 = "id: " + id
-        appendMessage(message1)
-    })
+  //display client's ID
+  socket.on("connect", () => {
+    id = socket.id;
+    socket.emit("sendID", id);
+  });
+  socket.on("idconnect", (id) => {
+    message1 = "id: " + id;
+    appendMessage(message1);
+  });
+  //send and receive messages
+  socket.on("receive-message", (message) => {
+    message1 = "received: " + message;
+    appendMessage(message1);
+    socket.emit("confirmReceived", message);
+  });
+  socket.on("getUsername", (name, message) => {
+    ChatService.getUserInfor().then((info) => {
+      if (info.username == name) {
+        socket.emit("confirmUsername", socket.id, message);
+        //socket.emit("sendMessage", socket.id + " success" + name)
+      } else {
+        socket.emit("sendMessage", socket.id + " fail" + name);
+      }
+    });
+  });
 
-    //send and receive messages
-    socket.on("receive-message", message => {
-        message1 = "received: " + message
-        appendMessage(message1)
-        socket.emit("confirmReceived", message)
-    })
+  function send(e) {
+    e.preventDefault();
+    appendMessage(e.target.m1.value);
+    socket.emit("sendMessageName", e.target.m1.value, e.target.join.value);
+  }
 
-    socket.on("getUsername", (name, message) => {
-        ChatService.getUserInfor()
-        .then((info) => {
-            if(info.username == name) {
-                socket.emit("confirmUsername", socket.id, message)
-                //socket.emit("sendMessage", socket.id + " success" + name)
-            } else {
-                socket.emit("sendMessage", socket.id + " fail" + name)
-            }
-        })
-    })
+  //join room
+  function joinRoom(e) {
+    e.preventDefault();
+    message1 = "join room: " + e.target.room.value;
+    appendMessage(message1);
+    socket.emit("joinRoom", e.target.room.value);
+  }
 
-    function send(e) {
-        e.preventDefault();
-        appendMessage(e.target.m1.value)
-        socket.emit('sendMessageName', e.target.m1.value, e.target.join.value)
-    }
+  //leave room
+  function leaveRoom(e) {
+    e.preventDefault();
+    message1 = "leave room: " + e.target.leave.value;
+    appendMessage(message1);
+    socket.emit("leaveRoom", e.target.leave.value);
+  }
+  function userConv(e) {
+    e.preventDefault();
+    ChatService.userConv(e.target.api.value)
+      .then((result) => {
+        message1 = result.data[0].members[0] + result.data[0].members[1];
+        appendMessage(message1);
 
-    //join room
-    function joinRoom(e) {
-        e.preventDefault();
-        message1 = "join room: " + e.target.room.value
-        appendMessage(message1)
-        socket.emit("joinRoom", e.target.room.value)
-    }
+        socket.emit("sendMessage", message1);
+      })
+      .catch((error) => {
+        socket.emit("sendMessage", error.message);
+      });
+  }
 
-    //leave room
-    function leaveRoom(e) {
-        e.preventDefault();
-        message1 = "leave room: " + e.target.leave.value
-        appendMessage(message1)
-        socket.emit("leaveRoom", e.target.leave.value)
-    }
-    function userConv(e) {
-        e.preventDefault();
-        ChatService.userConv(e.target.api.value)
-        .then((result) => {
-            message1 = result.data[0].members[0] + result.data[0].members[1]
-            appendMessage(message1)
-            
-            socket.emit('sendMessage', message1)
-        })
-        .catch((error) => {
-            socket.emit('sendMessage', error.message)
-        })
-    }
+  function getUserInfor(e) {
+    e.preventDefault();
+    ChatService.getUserInfor()
+      .then((user) => {
+        message1 = user.username;
+        appendMessage(message1);
+        socket.emit("sendMessage", message1);
+      })
+      .catch((error) => {
+        socket.emit("sendMessage", error.message);
+      });
+  }
 
-    function getUserInfor(e){
-        e.preventDefault();
-        ChatService.getUserInfor()
-        .then((user) => {
-            message1 = user.username
-            appendMessage(message1)
-            socket.emit('sendMessage',message1)
-        })
-        .catch((error) => {
-            socket.emit('sendMessage', error.message)
-        })
-    }
+  //display messages
+  function appendMessage(message) {
+    console.log("send");
+    messageContainer = document.getElementById("message-container");
+    const messageElement = document.createElement("div");
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+  }
 
-    //display messages
-    function appendMessage(message) {
-        console.log("send")
-        messageContainer = document.getElementById('message-container')
-        const messageElement = document.createElement('div');
-        messageElement.innerText = message;
-        messageContainer.append(messageElement);
-    }
+  //hotkeys for disconnect and reconnect (testing purpose)
+  document.addEventListener("keydown", (e) => {
+    if (e.target.matches("input")) return;
+    if (e.key === "c") socket.connect();
+    if (e.key === "d") socket.disconnect();
+  });
 
-
-    //hotkeys for disconnect and reconnect (testing purpose)
-    document.addEventListener('keydown', e => {
-        if (e.target.matches('input')) return
-        if (e.key === 'c') socket.connect()
-        if (e.key === 'd') socket.disconnect()
-    })
-
-
-    return (
-        <div>
-            {/* <div id="message-container"></div>
-            <form method="get" name="form1" id="form1" onSubmit={send}>
-                <input type="text" name="m1"/>
-                <button type="submit" form="form1" value="S1">Send Message To</button>
-                <input type="text" name="join"/>
-            </form>
-
-            <form method="get" name="form2" id="form2" onSubmit={joinRoom}>
-                <input type="text" name="room"/>
-                <button type="submit" form="form2" value="S2">join room</button>
-            </form>
-
-            <form method="get" name="form3" id="form3" onSubmit={leaveRoom}>
-                <input type="text" name="leave"/>
-                <button type="submit" form="form3" value="S3">leave room</button>
-            </form>
-            <form method="get" name="api" id="api" onSubmit={getUserInfor}>
-                <input type="text" name="api"/>
-                <button type="submit" form="api" value="S2">api test</button>
-            </form> */}
-            <Topbar />
-            <div className="messenger">
-                <div className="chatMenu">
-                    <div class="chatMenuWrapper">
-                        <input placeholder="search for someone" className="chatMenuInput"/>
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation /> 
-                    </div>
-                </div>
-                <div className="chatBox">
-                    <div class="chatBoxWrapper">
-                        <div className="chatBoxTop">
-                            <Message />
-                            <Message own={true}/>
-                            <Message />
-                            <Message />
-                            <Message own={true}/>
-                            <Message own={true}/>
-                            <Message />
-                            <Message own={true}/>
-                            <Message />
-                            <Message own={true}/>
-                            <Message own={true}/>
-                            <Message />
-                            <Message own={true}/>
-                        </div>
-                        <div className="chatBoxBottom">
-                            <textarea className="chatMessageInput" placeholder="write something"></textarea>
-                            <button className="chatSubmitButton">Send</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <div id="message-container"></div>
+      <form method="get" name="form1" id="form1" onSubmit={send}>
+        <input type="text" name="m1" />
+        <button type="submit" form="form1" value="S1">
+          Send Message To
+        </button>
+        <input type="text" name="join" />
+      </form>
+      <form method="get" name="api" id="api" onSubmit={getUserInfor}>
+        <input type="text" name="api" />
+        <button type="submit" form="api" value="S2">
+          api test
+        </button>
+      </form>
+      <Topbar />
+      <div className="messenger">
+        <div className="chatMenu">
+          <div className="chatMenuWrapper">
+            <input placeholder="search for someone" className="chatMenuInput" />
+            {/* <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation /> */}
+          </div>
         </div>
-    );
+        <div className="chatBox">
+          <div className="chatBoxWrapper">
+            <div className="chatBoxTop">
+              {/* <Message />
+              <Message own={true} />
+              <Message />
+              <Message />
+              <Message own={true} />
+              <Message own={true} />
+              <Message />
+              <Message own={true} />
+              <Message />
+              <Message own={true} />
+              <Message own={true} />
+              <Message />
+              <Message own={true} /> */}
+            </div>
+            <div className="chatBoxBottom">
+              <textarea
+                className="chatMessageInput"
+                placeholder="write something"
+              ></textarea>
+              <button className="chatSubmitButton">Send</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
-
