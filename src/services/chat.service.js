ea9af492bc0c6fe90568923454da3aa66960a3ce
QuanVo308 <62465205+QuanVo308@users.noreply.github.com>
2021-12-06 01:25:46 +0700

    message api

--- src/services/chat.service.js
+++ src/services/chat.service.js
@@ -10,6 +10,41 @@ class ChatService {
         const username = jwt(query.accessToken);
         return username
     }
+
+    async getMessage(sender, receiver) {
+        const res = await axios
+        .get(API_URL + "/api/chat/getMessage?sender=" + sender + "&receiver=" + receiver)
+        .then((res) => {
+            return res
+        })
+        return res
+    }
+
+    async getListMessager(sender) {
+        const res = await axios
+        .get(API_URL + "/api/chat/getListMessager?sender=" + sender)
+        .then((res) => {
+            return res
+        })
+        return res
+    }
+
+    async saveMessage(
+        _receiver,
+        _sender,
+        _text,
+    ) {
+        await axios.post(
+            API_URL + "/api/chat/saveMessage",
+            {
+              receiver: _receiver,
+              sender: _sender,
+              text: _text,
+            },
+          );
+        return
+    }
+
   }
   
   export default new ChatService();
